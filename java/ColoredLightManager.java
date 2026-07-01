e697a27a9a5091ef5fc6cd83e80bc7f3d42d322e
minSolly <mishamaganov@gmail.com>
2026-07-01 09:40:02 +0200

    Update ColoredLightManager.java

--- java/ColoredLightManager.java
+++ java/ColoredLightManager.java
@@ -1,8 +1,8 @@
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.BufferBuilder;
-import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.OpenGlHelper;
 import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.entity.Entity;
 import net.minecraft.tileentity.TileEntity;
